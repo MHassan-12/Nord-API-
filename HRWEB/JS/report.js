f8d29095dcd69b0b3e225ed1898cdb5f70d8e63b
MHassan-12 <bscs2312200@szabist.pk>
2025-05-09 05:29:30 +0000

    Create Script and HTML page

---
+++ HRWEB/JS/report.js
@@ -0,0 +1,17 @@
+/*
+const employee_link = "https://urban-couscous-7vw65wrgpgxjfxq96-6006.app.github.dev/employee"; 
+fetch(employee_link).then(response=>{
+    if(!response.ok){
+        throw new Error("Failed to fetch data from given URL")
+    }
+    return response.json();
+}).then(data=>{
+
+    document.getElementById('emp').innerHTML=data[0]
+   
+        tbody.appendChild(row);
+    });
+}).catch(err=>{
+    console.log(err.message);
+});
+*/
